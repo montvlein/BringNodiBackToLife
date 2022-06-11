f9f7d27c565b0a26b34b2e744a9300e4dcbac7cb
montlein <montvlein@gmail.com>
2022-06-11 15:50:08 -0400

    add Java Spring

---
+++ src/test/java/com/BringBackToLife/nodi/NodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BringBackToLife.nodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
