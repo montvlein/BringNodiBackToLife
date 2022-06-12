6f753cb013d80ce0e16066769751fd5d0f20a456
montlein <montvlein@gmail.com>
2022-06-12 01:42:26 -0400

    add properties, getters and setters

--- src/main/java/com/BringBackToLife/nodi/Models/Grupo.java
+++ src/main/java/com/BringBackToLife/nodi/Models/Grupo.java
@@ -1,4 +1,27 @@
 package com.BringBackToLife.nodi.Models;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Grupo {
+    private long ID;
+    private String nombre;
+    private Collection<Actividad> fechasImportantes = new ArrayList<>();
+    private Collection<Usuario> participantes = new ArrayList<>();
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
