206effb61416aef8ee830f1c37f8a6d30733518b
montlein <montvlein@gmail.com>
2022-06-12 01:50:24 -0400

    add properties, getters and setters

--- src/main/java/com/BringBackToLife/nodi/Models/Actividad.java
+++ src/main/java/com/BringBackToLife/nodi/Models/Actividad.java
@@ -1,4 +1,36 @@
 package com.BringBackToLife.nodi.Models;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 public class Actividad {
+    private long ID;
+    private String nombre;
+    private long ID_interes;
+    protected Collection<Evento> eventos = new ArrayList<>();
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setID_interes(long ID_interes) {
+        this.ID_interes = ID_interes;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public long getID_interes() {
+        return ID_interes;
+    }
+
 }
