b66cb36e3e8f9ff9a018a8c43c9d2769cc46a0ac
montlein <montvlein@gmail.com>
2022-06-12 11:40:52 -0400

    se agrego comentarios del porque no se usa

--- src/main/java/com/BringBackToLife/nodi/Models/Actividad.java
+++ src/main/java/com/BringBackToLife/nodi/Models/Actividad.java
@@ -3,6 +3,8 @@ package com.BringBackToLife.nodi.Models;
 import java.util.Collection;
 import java.util.ArrayList;
 
+// Esta clase se penso en un momento primer momento para agrupar eventos y encapsularse en los Grupos.
+// Pero por una cuestion de simplicidad, los Grupos guardan tienen una coleccion de Eventos. Dejando a esta clase sin uso.
 public class Actividad {
     private long ID;
     private String nombre;
