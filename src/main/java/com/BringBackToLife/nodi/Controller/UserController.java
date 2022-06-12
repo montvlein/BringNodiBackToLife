8bd85aa07cb3cf31da695fe11fce76b1b166417c
montlein <montvlein@gmail.com>
2022-06-12 14:08:42 -0400

    se agrego un metodo para devolver usuario por ID pasado como PathVariable

---
+++ src/main/java/com/BringBackToLife/nodi/Controller/UserController.java
@@ -0,0 +1,31 @@
+package com.BringBackToLife.nodi.Controller;
+
+import com.BringBackToLife.nodi.Models.Usuario;
+import com.BringBackToLife.nodi.Services.UsuarioServicios;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    private UsuarioServicios servicio = new UsuarioServicios();
+
+    @GetMapping("/v1/user/{userID}")
+    @ResponseBody
+    public ResponseEntity<Usuario> perfilDeUsuario(@PathVariable long userID) {
+        ResponseEntity<Usuario> response;
+        Usuario user = servicio.buscarUsuario(userID);
+        if (user != null) {
+            try {
+                response = ResponseEntity.ok(user);
+            } catch (Exception ex) {
+                response = ResponseEntity.badRequest().body(null);
+            }
+        }
+        else
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        return response;
+    }
+}
