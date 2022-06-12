691c4aa3172b7a2f6807f2eeb69e913c1094aca8
montlein <montvlein@gmail.com>
2022-06-12 14:02:57 -0400

    se agrego la estructura del metodo buscarUsuario, falta la logica

--- src/main/java/com/BringBackToLife/nodi/Services/UsuarioServicios.java
+++ src/main/java/com/BringBackToLife/nodi/Services/UsuarioServicios.java
@@ -1,10 +1,15 @@
 package com.BringBackToLife.nodi.Services;
 
+import com.BringBackToLife.nodi.Models.Usuario;
 import java.util.Collection;
 
 public class UsuarioServicios implements CUD{
 
     //TODO: completar servicios
+    public Usuario buscarUsuario(long id) {
+        return null;
+    }
+
     @Override
     public boolean agregarALaColeccion(Collection c, Object o) {
         return false;
