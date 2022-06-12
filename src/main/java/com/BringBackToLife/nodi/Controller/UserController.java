0c8654355c3ff232b6bdcfae6c96b3060610c60e
montlein <montvlein@gmail.com>
2022-06-12 14:09:22 -0400

    se agrego el atributo 'final' a la propiedad servicio

--- src/main/java/com/BringBackToLife/nodi/Controller/UserController.java
+++ src/main/java/com/BringBackToLife/nodi/Controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
-    private UsuarioServicios servicio = new UsuarioServicios();
+    private final UsuarioServicios servicio = new UsuarioServicios();
 
     @GetMapping("/v1/user/{userID}")
     @ResponseBody
