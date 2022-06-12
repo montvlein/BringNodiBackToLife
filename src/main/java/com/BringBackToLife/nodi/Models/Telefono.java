d056c1cd908b9ed1a82426374dbde202aa834ed7
montlein <montvlein@gmail.com>
2022-06-12 01:29:26 -0400

    add clase with properties, setter and  getters

---
+++ src/main/java/com/BringBackToLife/nodi/Models/Telefono.java
@@ -0,0 +1,29 @@
+package com.BringBackToLife.nodi.Models;
+
+import java.time.LocalDateTime;
+
+public class Telefono {
+    private long numero;
+    private LocalDateTime ultimoRegistro;
+
+    public Telefono(long numero, LocalDateTime ultimoRegistro) {
+        this.numero = numero;
+        this.ultimoRegistro = ultimoRegistro;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public LocalDateTime getUltimoRegistro() {
+        return ultimoRegistro;
+    }
+
+    public void setUltimoRegistro(LocalDateTime ultimoRegistro) {
+        this.ultimoRegistro = ultimoRegistro;
+    }
+}
