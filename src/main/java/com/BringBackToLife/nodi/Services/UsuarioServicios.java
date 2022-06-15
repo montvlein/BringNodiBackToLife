61fb54f19d6d70350987c85ff34642eed257d1c5
montlein <montvlein@gmail.com>
2022-06-15 13:07:44 -0400

    se elimino la implementacion del iDAO pero quedanlos metodos

--- src/main/java/com/BringBackToLife/nodi/Services/UsuarioServicios.java
+++ src/main/java/com/BringBackToLife/nodi/Services/UsuarioServicios.java
@@ -7,41 +7,34 @@ import com.BringBackToLife.nodi.Persistence.DAO.implement.UsuarioDAO;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class UsuarioServicios implements CUD, iDAO<Usuario>{
+public class UsuarioServicios implements CUD {
 
     iDAO<Usuario> dao = new UsuarioDAO();
 
-    @Override
     public boolean guardar(Usuario usuario) {
         return dao.guardar(usuario);
     }
 
-    @Override
     public boolean eliminar(long id) {
         return dao.eliminar(id);
     }
 
-    @Override
     public Usuario buscar(long id) {
         return dao.buscar(id);
     }
 
-    @Override
     public Usuario actualizar(Usuario usuario) {
         return dao.actualizar(usuario);
     }
 
-    @Override
     public ArrayList<Usuario> listarTodos() {
         return dao.listarTodos();
     }
 
-    @Override
     public boolean agregarALaColeccion(Collection c, Object o) {
         return false;
     }
 
-    @Override
     public boolean eliminarDeLaColeccion(Collection c, Object o) {
         return false;
     }
