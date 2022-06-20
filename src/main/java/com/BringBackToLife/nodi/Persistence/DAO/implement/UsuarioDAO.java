58ef543029f15ef08dc4871052be13c0db17fba4
montlein <montvlein@gmail.com>
2022-06-20 18:58:57 -0400

    se implementaron los metodos CRUD para guardar en memoria

--- src/main/java/com/BringBackToLife/nodi/Persistence/DAO/implement/UsuarioDAO.java
+++ src/main/java/com/BringBackToLife/nodi/Persistence/DAO/implement/UsuarioDAO.java
@@ -2,33 +2,42 @@ package com.BringBackToLife.nodi.Persistence.DAO.implement;
 
 import com.BringBackToLife.nodi.Models.Usuario;
 import com.BringBackToLife.nodi.Persistence.DAO.iDAO;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
+@Repository
 public class UsuarioDAO implements iDAO<Usuario> {
+
+    private final HashMap<Long, Usuario> datosEnMemoria = new HashMap<>();
 
     @Override
     public boolean guardar(Usuario usuario) {
-        return false;
+        datosEnMemoria.put(usuario.getId(),usuario);
+        return true;
     }
 
     @Override
     public boolean eliminar(long id) {
-        return false;
+        datosEnMemoria.remove(id);
+        return true;
     }
 
     @Override
     public Usuario buscar(long id) {
-        return new Usuario();
+        return datosEnMemoria.get(id);
     }
 
     @Override
     public Usuario actualizar(Usuario usuario) {
-        return new Usuario();
+        datosEnMemoria.remove(usuario.getId());
+        return datosEnMemoria.put(usuario.getId(), usuario);
     }
 
     @Override
     public ArrayList<Usuario> listarTodos() {
-        return null;
+        return (ArrayList<Usuario>) datosEnMemoria.values();
     }
 }
