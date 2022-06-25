c7d61e5e5789d67c931eeb0562949739752f153c
montlein <montvlein@gmail.com>
2022-06-24 23:06:40 -0400

    se elimino CUD

--- src/main/java/com/BringBackToLife/nodi/Services/CUD.java
+++
@@ -1,8 +0,0 @@
-package com.BringBackToLife.nodi.Services;
-
-import java.util.Collection;
-
-public interface CUD<T> {
-    boolean agregarALaColeccion(Collection<T> c, T t);
-    boolean eliminarDeLaColeccion(Collection<T> c, T t);
-}
--- src/main/java/com/BringBackToLife/nodi/Services/GrupoServicios.java
+++ src/main/java/com/BringBackToLife/nodi/Services/GrupoServicios.java
@@ -4,17 +4,6 @@ import com.BringBackToLife.nodi.Models.Evento;
 
 import java.util.Collection;
 
-public class GrupoServicios implements CUD<Evento> {
+public class GrupoServicios {
 
-    //TODO: completar servicios
-
-    @Override
-    public boolean agregarALaColeccion(Collection<Evento> c, Evento evento) {
-        return false;
-    }
-
-    @Override
-    public boolean eliminarDeLaColeccion(Collection<Evento> c, Evento evento) {
-        return false;
-    }
 }
