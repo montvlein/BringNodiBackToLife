5de898f1452abd10d67396d5a1d7bcc76916f5e0
montvlein <montvlein@gmail.com>
2022-06-25 17:37:39 -0300

    se creo un controller para renderizar la vista

--- src/main/java/com/BringBackToLife/nodi/Controller/IndexController.java
+++ src/main/java/com/BringBackToLife/nodi/Controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.BringBackToLife.nodi.Controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
