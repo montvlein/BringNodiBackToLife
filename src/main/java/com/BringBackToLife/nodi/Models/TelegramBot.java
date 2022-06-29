5d5c2b5d1b8a410220b7974655b6ce5239eeee1f
montlein <montvlein@gmail.com>
2022-06-29 01:09:12 -0400

    se implemento bot de telegram

---
+++ src/main/java/com/BringBackToLife/nodi/Models/TelegramBot.java
@@ -0,0 +1,40 @@
+package com.BringBackToLife.nodi.Models;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class TelegramBot {
+
+    private final String botUri ="https://api.telegram.org/bot";
+
+    @Value("${telegram.token}")
+    private String token;
+
+    @Value("${telegram.chatId}")
+    private String chat_id;
+
+    RestTemplate restTemplate;
+    HttpHeaders headers;
+
+    public String getMe(){
+        restTemplate = new RestTemplate();
+        return restTemplate.getForObject(botUri+token + "/getMe", String.class);
+    }
+
+    public String sendMessage(String msg) {
+        restTemplate = new RestTemplate();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject json = new JSONObject();
+        json.put("chat_id", chat_id);
+        json.put("text",msg);
+        HttpEntity<String> request = new HttpEntity<>(json.toString() ,headers);
+        return restTemplate.postForObject(botUri+token+"/sendMesssage", request, String.class);
+    }
+}
